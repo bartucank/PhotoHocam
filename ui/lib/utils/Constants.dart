e601ce52035125281f4d2c58c887956bd0a5450b
bartucank <bartucankk@gmail.com>
2024-01-22 21:26:11 +0200

    Docs.

--- ui/lib/utils/Constants.dart
+++ ui/lib/utils/Constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  // static const String apiBaseUrl = "http://cloud495-407311.oa.r.appspot.com";
-  static const String apiBaseUrl = "https://a732-78-135-49-15.ngrok-free.app";
+  static const String apiBaseUrl = "http://cloud495-407311.oa.r.appspot.com";
+  // static const String apiBaseUrl = "https://22ee-78-135-49-15.ngrok-free.app";
 }
